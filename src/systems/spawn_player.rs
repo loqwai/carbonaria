09295fa875cecef2a436627f41751566a8fc9977
Roy van de Water <roy@circleci.com>
2022-08-29 16:01:19 -0700

    Shrug

--- src/systems/spawn_player.rs
+++ src/systems/spawn_player.rs
@@ -2,9 +2,15 @@ use bevy::prelude::*;
 
 use crate::bundles::{PlayerBundle, StickBundle};
 
-pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, animations: ResMut<Assets<AnimationClip>>) {
+pub fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    animations: ResMut<Assets<AnimationClip>>,
+) {
     let player = commands.spawn_bundle(PlayerBundle::new(&asset_server)).id();
-    let stick = commands.spawn_bundle(StickBundle::new(&asset_server, animations)).id();
+    let stick = commands
+        .spawn_bundle(StickBundle::new(&asset_server, animations))
+        .id();
 
     commands.entity(player).push_children(&[stick]);
 }
