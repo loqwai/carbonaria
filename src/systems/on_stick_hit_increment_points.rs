0f7eb2d65ef287a47cfa2aa6049d4525f47f7a49
Roy van de Water <roy@circleci.com>
2022-08-29 18:20:53 -0700

    Points only if stick is swinging

--- src/systems/on_stick_hit_increment_points.rs
+++ src/systems/on_stick_hit_increment_points.rs
@@ -5,7 +5,7 @@ use crate::components::{Mob, Player, Points, Stick};
 
 pub fn on_stick_hit_increment_points(
     mut q_points: Query<&mut Points, With<Player>>,
-    q_sticks: Query<&Stick>,
+    q_sticks: Query<&AnimationPlayer, With<Stick>>,
     q_mobs: Query<&Mob>,
     mut events: EventReader<CollisionEvent>,
 ) {
@@ -15,15 +15,15 @@ pub fn on_stick_hit_increment_points(
             CollisionEvent::Started(t1, t2) => Some((t1, t2)),
             CollisionEvent::Stopped(_, _) => None,
         })
-        .filter(|(t1, t2)| {
-            if let Err(_) = q_sticks.get(t1.rigid_body_entity()) {
-                return false;
-            }
-            if let Err(_) = q_mobs.get(t2.rigid_body_entity()) {
+        .filter(|(maybe_stick, maybe_mob)| {
+            if let Err(_) = q_mobs.get(maybe_mob.rigid_body_entity()) {
                 return false;
             }
 
-            return true;
+            match q_sticks.get(maybe_stick.rigid_body_entity()) {
+                Err(_) => false,
+                Ok(animation_player) => !animation_player.is_paused(),
+            }
         })
     {
         for mut points in q_points.iter_mut() {
