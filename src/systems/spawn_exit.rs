39eb583f6ca5d6072c86a171c308497ad0e241ab
redaphid <iam@aaronherres.com>
2022-10-04 17:08:23 -0700

    add de/respawn exits

--- src/systems/spawn_exit.rs
+++ src/systems/spawn_exit.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use rand::rngs::SmallRng;
 
 use crate::bundles::ExitBundle;
+use crate::components::Exit;
 use crate::resources::Config;
 use crate::util::random_position;
 
@@ -10,7 +11,11 @@ pub fn spawn_exit(
     asset_server: Res<AssetServer>,
     config: Res<Config>,
     mut rng: ResMut<SmallRng>,
+    existing_exits: Query<Entity, With<Exit>>
 ) {
+    if !existing_exits.is_empty() {
+        return;
+    }
     let position = random_position(&config, &mut rng);
 
     commands.spawn_bundle(ExitBundle::new(&asset_server, position));
