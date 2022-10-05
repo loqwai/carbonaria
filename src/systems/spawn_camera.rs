d1e7af52055ebcdee5b3c3f8c8fdbcbba3a04c9c
Roy van de Water <roy.v.water@gmail.com>
2022-10-05 07:16:28 -0700

    Nvm, textures are still weird

--- src/systems/spawn_camera.rs
+++ src/systems/spawn_camera.rs
@@ -1,5 +1,5 @@
-use bevy::prelude::{Camera2dBundle, Commands};
+use bevy::prelude::*;
 
 pub fn spawn_camera(mut commands: Commands) {
-    commands.spawn_bundle(Camera2dBundle::new_with_far(10.0));
+    commands.spawn_bundle(Camera2dBundle::new_with_far(100.0));
 }
