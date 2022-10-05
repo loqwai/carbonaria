3d7aa5e6544c03f5654e6a6491a2b96737ceb39c
Roy van de Water <roy.v.water@gmail.com>
2022-10-05 07:21:10 -0700

    Only player can trigger the exit

--- src/systems/detect_exit.rs
+++ src/systems/detect_exit.rs
@@ -1,18 +1,24 @@
 use bevy::prelude::*;
 use heron::Collisions;
 
-use crate::components::Exit;
+use crate::components::{Exit, Player};
 use crate::events::ResetEvent;
 
 pub fn detect_exit(
     q_exits: Query<&Collisions, With<Exit>>,
-    mut reset_event_writer: EventWriter<ResetEvent>
+    q_players: Query<Entity, With<Player>>,
+    mut reset_event_writer: EventWriter<ResetEvent>,
 ) {
-    let exit_happened = q_exits.iter().any(|collisions| !collisions.is_empty());
+    match q_players.get_single() {
+        Err(_) => return,
+        Ok(player) => {
+            let exit_happened = q_exits.iter().any(|collisions| collisions.contains(&player));
 
-    if !exit_happened {
-        return;
+            if !exit_happened {
+                return;
+            }
+
+            reset_event_writer.send(ResetEvent {});
+        }
     }
-
-    reset_event_writer.send(ResetEvent {});
 }
