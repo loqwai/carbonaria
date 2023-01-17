348827c6aac3302e52ecfb8387834cc4f97b17de
redaphid <iam@aaronherres.com>
2023-01-17 16:49:12 -0700

    we can change teams now

---
+++ src/systems/spawn_powerups.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+use crate::{bundles::ChestBundle, components::Team};
+
+pub fn spawn_powerups(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let team_powerup = commands.spawn().insert(Team(1)).id();
+    commands.spawn_bundle(ChestBundle::new(
+        &asset_server,
+        Vec3::new(-128.0, 0.0, 0.0),
+        team_powerup,
+    ));
+}
