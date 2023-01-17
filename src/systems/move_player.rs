4732fd04073601e897b148217865833743733cca
redaphid <iam@aaronherres.com>
2023-01-17 15:09:10 -0700

    move move player to events

--- src/systems/move_player.rs
+++ src/systems/move_player.rs
@@ -26,36 +26,28 @@ fn fallible_move_player(
     mut move_events: EventWriter<MoveEvent>,
 ) -> Result<(), MovePlayerError> {
     for (entity, _) in velocity_query.iter_mut() {
-        let mut entity_speed: f32 = 40.0;
-
-        for (speed, parent) in speed_query.iter() {
-            if parent.id() != entity.id() { continue }
-
-            entity_speed *= speed.0;
-        }
-
         if keyboard_input.pressed(KeyCode::A) {
             move_events.send(MoveEvent{
                 target: entity,
-                velocity: Vec3::new(-entity_speed, 0.0, 0.0),
+                velocity: Vec3::new(-1.0, 0.0, 0.0),
             });
         }
         if keyboard_input.pressed(KeyCode::D) {
             move_events.send(MoveEvent{
                 target: entity,
-                velocity: Vec3::new(entity_speed, 0.0, 0.0),
+                velocity: Vec3::new(1.0, 0.0, 0.0),
             });
         }
         if keyboard_input.pressed(KeyCode::W) {
             move_events.send(MoveEvent{
                 target: entity,
-                velocity: Vec3::new(0.0, entity_speed, 0.0),
+                velocity: Vec3::new(0.0, 1.0, 0.0),
             });
         }
         if keyboard_input.pressed(KeyCode::S) {
             move_events.send(MoveEvent{
                 target: entity,
-                velocity: Vec3::new(0.0, -entity_speed, 0.0),
+                velocity: Vec3::new(0.0, -1.0, 0.0),
             });
         }
     }
--- src/systems/move_thing.rs
+++ src/systems/move_thing.rs
@@ -1,12 +1,22 @@
 use bevy::prelude::*;
 use heron::prelude::*;
 
-use crate::events::MoveEvent;
+use crate::{events::MoveEvent, components::Speed};
 
-pub fn move_thing(mut moveable: Query<&mut Velocity>, mut move_events: EventReader<MoveEvent>) {
+pub fn move_thing(mut moveable: Query<(&mut Velocity, &Speed)>, speed_query: Query<(&Speed, &Parent)>, mut move_events: EventReader<MoveEvent>) {
     move_events.iter().for_each(|event| {
         match moveable.get_mut(event.target) {
-            Ok(mut velocity) => velocity.linear += event.velocity,
+            Ok((mut velocity, own_speed)) => {
+                let mut entity_speed: f32 = own_speed.0;
+
+                for (speed, parent) in speed_query.iter() {
+                    if parent.id() != event.target.id() { continue }
+
+                    entity_speed *= speed.0;
+                }
+
+                velocity.linear += event.velocity * entity_speed;
+            }
             Err(_) => return,
         };
     });
