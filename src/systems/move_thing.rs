b2e154bf0c7c4c2d0d809376e1eff8fcb247b097
Roy van de Water <roy.v.water@gmail.com>
2023-01-18 05:52:18 -0700

    Movement works as expected, but powerup pickup seems broken

--- src/systems/move_thing.rs
+++ src/systems/move_thing.rs
@@ -1,15 +1,21 @@
 use bevy::prelude::*;
 
-use crate::{events::MoveEvent, components::Speed};
+use crate::{components::Speed, events::MoveEvent};
 
-pub fn move_thing(mut moveable: Query<(&mut Transform, &Speed)>, speed_query: Query<(&Speed, &Parent)>, mut move_events: EventReader<MoveEvent>) {
+pub fn move_thing(
+    mut moveable: Query<(&mut Transform, &Speed)>,
+    speed_query: Query<(&Speed, &Parent)>,
+    mut move_events: EventReader<MoveEvent>,
+) {
     move_events.iter().for_each(|event| {
         match moveable.get_mut(event.who) {
             Ok((mut transform, own_speed)) => {
                 let mut entity_speed: f32 = own_speed.0;
 
                 for (speed, parent) in speed_query.iter() {
-                    if parent.id() != event.who.id() { continue }
+                    if parent.get() != event.who {
+                        continue;
+                    }
 
                     entity_speed *= speed.0;
                 }
--- src/systems/spawn_powerups.rs
+++ src/systems/spawn_powerups.rs
@@ -1,9 +1,9 @@
-use bevy::prelude::*;
 use crate::{bundles::ChestBundle, components::Team};
+use bevy::prelude::*;
 
 pub fn spawn_powerups(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let team_powerup = commands.spawn().insert(Team(0)).id();
-    commands.spawn_bundle(ChestBundle::new(
+    let team_powerup = commands.spawn_empty().insert(Team(0)).id();
+    commands.spawn(ChestBundle::new(
         &asset_server,
         Vec3::new(-128.0, 0.0, 0.0),
         team_powerup,
