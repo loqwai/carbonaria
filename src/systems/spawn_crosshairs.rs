f1c8fca95f57f16eaea2f566cbc501a8d3eaf9d1
redaphid <iam@aaronherres.com>
2023-01-19 20:50:27 -0700

    crosshairs

--- src/systems/spawn_crosshairs.rs
+++ src/systems/spawn_crosshairs.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use crate::components::MousePos;
 pub fn spawn_crosshairs(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("mob.png"),
+        texture: asset_server.load("crosshairs.png"),
         transform: Transform::from_xyz(0.0, 0.0, 1.0),
         ..Default::default()
     }).insert(MousePos);
