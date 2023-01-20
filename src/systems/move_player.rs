00ebda781853ffdae15b49c1f57a2519d8c6af8b
redaphid <iam@aaronherres.com>
2023-01-19 23:29:23 -0700

    have player look at cursor via some unorganized code

--- src/systems/move_player.rs
+++ src/systems/move_player.rs
@@ -1,7 +1,10 @@
-use bevy::{ecs::query::QuerySingleError, input::Input, prelude::*};
-use bevy_rapier2d::prelude::Velocity;
+use bevy::{prelude::*, ecs::query::QuerySingleError};
+use bevy_rapier2d::{prelude::Velocity};
 
-use crate::{components::Player, events::MoveEvent};
+use crate::{
+    components::{MousePos, Player},
+    events::MoveEvent, util::look_at_target,
+};
 
 #[derive(Debug, Error)]
 enum MovePlayerError {
@@ -10,46 +13,60 @@ enum MovePlayerError {
 
 pub fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    velocity_query: Query<(Entity, &mut Velocity), With<Player>>,
+    velocity_query: Query<(Entity, &Transform, &mut Velocity), With<Player>>,
+    mouse_query: Query<&Transform, With<MousePos>>,
     move_events: EventWriter<MoveEvent>,
 ) {
-    if let Err(e) = fallible_move_player(keyboard_input, velocity_query, move_events) {
+    if let Err(e) = fallible_move_player(
+        keyboard_input,
+        velocity_query,
+        mouse_query,
+        move_events) {
         println!("Error moving player: {}", e);
     }
 }
 
 fn fallible_move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    mut velocity_query: Query<(Entity, &mut Velocity), With<Player>>,
+    mut velocity_query: Query<(Entity, &Transform, &mut Velocity), With<Player>>,
+    mouse_query: Query<&Transform, With<MousePos>>,
     mut move_events: EventWriter<MoveEvent>,
 ) -> Result<(), MovePlayerError> {
-    for (entity, _) in velocity_query.iter_mut() {
+    for (entity,player_pos, _) in velocity_query.iter_mut() {
+        let mut rotation = Quat::default();
+        match mouse_query.get_single() {
+            Ok(mouse_pos) => {
+                let (r,_) = look_at_target(player_pos.translation, mouse_pos.translation);
+                rotation = r;
+            },
+            Err(_) => {todo!()},
+        }
         if keyboard_input.pressed(KeyCode::A) {
             move_events.send(MoveEvent {
                 who: entity,
                 velocity: Vec3::new(-1.0, 0.0, 0.0),
-                rotation: Quat::default(),
+                rotation,
             });
         }
         if keyboard_input.pressed(KeyCode::D) {
             move_events.send(MoveEvent {
                 who: entity,
                 velocity: Vec3::new(1.0, 0.0, 0.0),
-                rotation: Quat::default(),
+                rotation,
             });
         }
         if keyboard_input.pressed(KeyCode::W) {
             move_events.send(MoveEvent {
                 who: entity,
                 velocity: Vec3::new(0.0, 1.0, 0.0),
-                rotation: Quat::default(),
+                rotation,
             });
         }
         if keyboard_input.pressed(KeyCode::S) {
             move_events.send(MoveEvent {
                 who: entity,
                 velocity: Vec3::new(0.0, -1.0, 0.0),
-                rotation: Quat::default(),
+                rotation,
             });
         }
     }
