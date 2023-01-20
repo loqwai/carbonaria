c45ca8ea17d762176272b0dd5159397d2691dd16
redaphid <iam@aaronherres.com>
2023-01-19 22:54:47 -0700

    move rotation/direction logic into a function to prep for cursor

--- src/systems/chasers_follow_other_teams.rs
+++ src/systems/chasers_follow_other_teams.rs
@@ -13,15 +13,19 @@ pub fn chasers_follow_other_teams(
         match targets.iter().find(|(team, _)| team != &chaser_team) {
             None => return,
             Some((_, target_transform)) => {
-                let diff =  chaser_transform.translation - target_transform.translation;
-                let angle = diff.y.atan2(diff.x); // Add/sub FRAC_PI here optionally
-
+                let (rotation,direction) = look_at_target(chaser_transform.translation, target_transform.translation);
                 move_events.send(MoveEvent{
                     who: chaser_entity,
-                    velocity: diff.normalize() * -1.0,
-                    rotation: Quat::from_axis_angle(Vec3::Z, angle),
+                    velocity: direction,
+                    rotation,
                 });
             }
         }
     });
+}
+pub fn look_at_target(looker: Vec3, target: Vec3) -> (Quat, Vec3) {
+    let diff = looker - target;
+    let angle = diff.y.atan2(diff.x); // Add/sub FRAC_PI here optionally
+    let rotation = Quat::from_axis_angle(Vec3::Z, angle);
+    return (rotation, diff.normalize()*-1.0);
 }
