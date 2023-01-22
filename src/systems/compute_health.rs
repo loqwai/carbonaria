eea0f409349b57875760cfc6b64c89ae6547c684
redaphid <iam@aaronherres.com>
2023-01-21 19:03:45 -0700

    check if entity that is modified has a modifiable property, and ignore if it doesn't

--- src/systems/compute_health.rs
+++ src/systems/compute_health.rs
@@ -8,8 +8,14 @@ pub fn compute_health(
 ) {
     modifiers.for_each(|(mods, modifies)| {
         let target = modifies.0;
-        let mut health = healthies.get_component_mut::<Health>(target).unwrap();
-        health.0 += mods.0;
+        match healthies.get_component_mut::<Health>(target) {
+            Ok(mut health) => {
+                health.0 += mods.0;
+            }
+            Err(e) => {
+                println!("Entity {:?} does not have a Health component", target);
+            }
+        }
     });
 }
 
@@ -39,4 +45,22 @@ fn did_add_health() {
     // Check resulting changes
     let hp = app.world.get::<Health>(player).unwrap();
     assert_eq!(hp.0, -10);
+}
+
+#[test]
+fn modifying_health_for_entities_without_health_is_ignored() {
+    // Setup app
+    let mut app = App::new();
+
+    // Add our two systems
+    app.add_system(reset_computed_health_to_0.before(compute_health));
+    app.add_system(compute_health);
+
+    // Setup test entities
+    let player = app.world.spawn_empty().id();
+
+    app.world.spawn((Health(10), Modifier, Modifies(player)));
+    app.world.spawn((Health(-20), Modifier, Modifies(player)));
+    // Run systems
+    app.update();
 }
