8649de5bef0c4eac25ead63d7c7fd65cdc746f4e
Roy van de Water <roy.v.water@gmail.com>
2023-01-25 11:54:18 -0700

    Allow things without children to move

--- src/systems/move_thing.rs
+++ src/systems/move_thing.rs
@@ -1,9 +1,15 @@
 use bevy::prelude::*;
 
-use crate::{components::Speed, events::MoveEvent};
+use crate::{
+    components::{LaserGunBullet, Speed},
+    events::MoveEvent,
+};
 
 pub fn move_thing(
-    mut moveable: Query<(&mut Transform, &Speed, &Children)>,
+    mut moveable: Query<(&mut Transform, &Speed)>,
+    // seperate children so we can still move things without children
+    moveable_children: Query<&Children, (With<Transform>, With<Speed>)>,
+
     speed_query: Query<&Speed>,
     mut move_events: EventReader<MoveEvent>,
 ) {
@@ -14,17 +20,35 @@ pub fn move_thing(
 
         match moveable.get_mut(event.who) {
             Err(_) => return,
-            Ok((mut transform, own_speed, children)) => {
+            Ok((mut transform, own_speed)) => {
                 let entity_speed: f32 = own_speed.0;
-
-                let speed_multiplier: f32 = children
-                    .iter()
-                    .filter_map(|&child| speed_query.get(child).ok().map(|speed| speed.0))
-                    .product();
+                let speed_multiplier =
+                    get_speed_multiplier(&moveable_children, &speed_query, event.who)
+                        .unwrap_or(1.0);
 
                 transform.translation +=
                     event.direction.normalize() * entity_speed * speed_multiplier;
             }
         };
     });
+}
+
+fn get_speed_multiplier(
+    moveable_children: &Query<&Children, (With<Transform>, With<Speed>)>,
+    speed_query: &Query<&Speed>,
+    parent: Entity,
+) -> Option<f32> {
+    let children = moveable_children.get(parent).ok()?;
+
+    if children.iter().count() == 0 {
+        return None;
+    }
+
+    Some(
+        children
+            .iter()
+            .filter_map(|&child| speed_query.get(child).ok())
+            .map(|speed| speed.0)
+            .product(),
+    )
 }
