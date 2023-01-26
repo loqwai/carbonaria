35f44b941ae3780ee1051bc2a3663aeb3b4c3a9f
Roy van de Water <roy.v.water@gmail.com>
2023-01-26 12:01:07 -0700

    Fix WASM build

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod systems;
 mod util;
 
 use bevy::{prelude::*, time::FixedTimestep};
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+// use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier2d::prelude::*;
 
 use components::Tick;
@@ -66,7 +66,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugin(RapierDebugRenderPlugin::default()) // the physics debug UI
-        .add_plugin(WorldInspectorPlugin)
+        // .add_plugin(WorldInspectorPlugin)
         .insert_resource(RapierConfiguration {
             gravity: Vec2::ZERO,
             ..Default::default()
--- wasm.sh
+++ wasm.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
+prerequisit_wasm32() {
+  rustup show | grep wasm32-unknown-unknown &> /dev/null
+
+  if [ $? == 0 ]; then
+    return 0;
+  fi
+
+  echo "rust WASM toolchain not installed. Run the following command to install:"
+  echo ""
+  echo "rustup target install wasm32-unknown-unknown"
+  return 1
+}
+
+prerequisit_wasm_bindgen() {
+  wasm-bindgen --version &> /dev/null
+
+  if [ $? == 0 ]; then
+    return 0;
+  fi
+
+  echo "wasm-bindgen not installed. Run the following command to install:"
+  echo ""
+  echo "cargo install wasm-bindgen-cli"
+  return 1
+}
+
+prerequisit_basic_http_server() {
+  basic-http-server --version &> /dev/null
+
+  if [ $? == 0 ]; then
+    return 0;
+  fi
+
+  echo "basic-http-server not installed. Run the following command to install:"
+  echo ""
+  echo "cargo install basic-http-server"
+  return 1
+}
+
+prerequisits() {
+  prerequisit_wasm32 && prerequisit_wasm_bindgen && prerequisit_basic_http_server
+}
+
 build() {
   cargo build \
     --release \
+    --no-default-features \
     --target wasm32-unknown-unknown
 }
 
@@ -14,13 +58,20 @@ bindgen() {
     target/wasm32-unknown-unknown/release/carbonaria.wasm
 }
 
+copy_assets() {
+  cp assets/index.html target/wasm/index.html
+  cp -r assets target/wasm/
+}
+
 run_server() {
   basic-http-server target/wasm
 }
 
 main(){
-  build \
+  prerequisits \
+  && build \
   && bindgen \
+  && copy_assets \
   && run_server
 }
 main "$@"
