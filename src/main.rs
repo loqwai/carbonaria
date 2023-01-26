b6a1edc4eb0eee989018aa6595957699c6be0666
redaphid <iam@aaronherres.com>
2023-01-25 17:14:07 -0700

    group ui systems & game loop systems

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,39 @@ mod resources;
 mod systems;
 mod util;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, time::FixedTimestep};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier2d::prelude::*;
 
 use resources::{Config, MobSpawnTimer, SmallRng};
 
+const TIME_STEP: f32 = 1.0 / 60.0; //rapier runs at 60fps by default.
 fn main() {
-    let uiSystemSet = SystemSet::new()
+    // group ui systems together bc we want to run them as fast as possible
+    let ui_system_set = SystemSet::new()
         .with_system(systems::update_compass)
         .with_system(systems::update_score_ui)
         .with_system(systems::update_health_ui)
         .with_system(systems::sync_mouse_position)
         .with_system(systems::player_aimables_aim_at_cursor)
-        .with_system(systems::follow_player_with_camera)
-        ;
+        .with_system(systems::follow_player_with_camera);
+
+    let game_loop_system_set = SystemSet::new()
+        .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+        .with_system(systems::shoot_gun)
+        .with_system(systems::move_bullet)
+        .with_system(systems::spawn_exit)
+        .with_system(systems::spawn_mobs)
+        .with_system(systems::chasers_follow_other_teams)
+        .with_system(systems::move_player)
+        .with_system(systems::move_thing)
+        .with_system(systems::rotate_thing)
+        .with_system(systems::detect_exit)
+        .with_system(systems::detect_damager_hits)
+        .with_system(systems::team_powerup_assigns_team)
+        .with_system(systems::on_0_health_kill)
+        .with_system(systems::on_chest_hit_pickup)
+        .with_system(systems::on_damager_hit_subtract_health);
 
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
@@ -53,20 +71,7 @@ fn main() {
         .add_startup_system(systems::spawn_wallbreaker_chest)
         .add_startup_system(systems::spawn_powerups)
         .add_startup_system(systems::spawn_crosshairs)
-        .add_system_set(uiSystemSet)
-        .add_system(systems::shoot_gun)
-        .add_system(systems::move_bullet)
-        .add_system(systems::spawn_exit)
-        .add_system(systems::spawn_mobs)
-        .add_system(systems::chasers_follow_other_teams)
-        .add_system(systems::move_player)
-        .add_system(systems::move_thing)
-        .add_system(systems::rotate_thing)
-        .add_system(systems::detect_exit)
-        .add_system(systems::detect_damager_hits)
-        .add_system(systems::team_powerup_assigns_team)
-        .add_system(systems::on_0_health_kill)
-        .add_system(systems::on_chest_hit_pickup)
-        .add_system(systems::on_damager_hit_subtract_health)
+        .add_system_set(ui_system_set)
+        .add_system_set(game_loop_system_set)
         .run();
 }
