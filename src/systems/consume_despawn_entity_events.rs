49bb958043a0c03a5005f8ae8d0dfb8708188159
Roy van de Water <roy.v.water@gmail.com>
2023-02-03 16:33:49 -0700

    Refactor despawn

--- src/systems/consume_despawn_entity_events.rs
+++ src/systems/consume_despawn_entity_events.rs
@@ -1,15 +1,13 @@
-use bevy::prelude::*;
 use crate::events::DespawnEvent;
+use bevy::prelude::*;
 
 pub fn consume_despawn_entity_events(
     mut despawn_events: EventReader<DespawnEvent>,
     mut commands: Commands,
     entities: Query<Entity>,
-){
-    despawn_events.iter().for_each(|event| {
-        match entities.get(event.entity).ok() {
-            None => (),
-            Some(entity) => commands.entity(entity).despawn_recursive(),
-        }
-    })
+) {
+    despawn_events
+        .iter()
+        .filter_map(|event| entities.get(event.entity).ok())
+        .for_each(|entity| commands.entity(entity).despawn_recursive())
 }
