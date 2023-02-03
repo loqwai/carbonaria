db1bd11dec4056c624fa8750a18d53412e3c041e
Roy van de Water <roy.v.water@gmail.com>
2023-02-03 15:38:14 -0700

    Add 8 secret, hidden angles to the player sprite sheet

--- src/bundles/player_bundle.rs
+++ src/bundles/player_bundle.rs
@@ -30,7 +30,7 @@ impl PlayerBundle {
         scale: f32,
     ) -> PlayerBundle {
         let texture = asset_server.load("player-sprite-sheet.png");
-        let texture_atlas = TextureAtlas::from_grid(texture, Vec2::new(512.0, 512.0), 4, 2, None, None);
+        let texture_atlas = TextureAtlas::from_grid(texture, Vec2::new(512.0, 512.0), 4, 4, None, None);
         let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
         PlayerBundle {
