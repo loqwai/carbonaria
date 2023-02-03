7cfe503fe0c0806847e2d9122e28ed02b995dd49
Roy van de Water <roy.v.water@gmail.com>
2023-02-03 16:27:29 -0700

    Check if entity exists before despawn to silence error

--- src/systems/consume_despawn_entity_events.rs
+++ src/systems/consume_despawn_entity_events.rs
@@ -4,8 +4,12 @@ use crate::events::DespawnEvent;
 pub fn consume_despawn_entity_events(
     mut despawn_events: EventReader<DespawnEvent>,
     mut commands: Commands,
+    entities: Query<Entity>,
 ){
     despawn_events.iter().for_each(|event| {
-        commands.entity(event.entity).despawn_recursive();
+        match entities.get(event.entity).ok() {
+            None => (),
+            Some(entity) => commands.entity(entity).despawn_recursive(),
+        }
     })
 }
