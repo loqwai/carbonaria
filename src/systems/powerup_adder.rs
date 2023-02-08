6c4de441168c0643a53e9c7145422dc0c52eceba
redaphid <iam@aaronherres.com>
2023-02-07 22:39:07 -0700

    Basically works, but only via copying bc I don't understand lifetimes in Rust

--- src/systems/powerup_adder.rs
+++ src/systems/powerup_adder.rs
@@ -3,43 +3,54 @@ use std::ops::{Add, AddAssign};
 use bevy::prelude::*;
 
 #[derive(Component, Debug)]
-pub struct Powerup<T:Component> {
+pub struct Powerup<T: Component + AddAssign + Copy> {
     pub value: T,
 }
-#[derive(Component,Debug)]
+#[derive(Component, Debug,Copy)]
 pub struct RateOfFire(pub usize);
+impl AddAssign for RateOfFire {
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+    }
+}
+impl Clone for RateOfFire {
+    fn clone(&self) -> Self {
+        RateOfFire(self.0)
+    }
+}
 
-pub fn powerup_adder(
-    powerups: Query<(&Parent,&Powerup<RateOfFire>)>,
-    mut guns: Query<&mut RateOfFire>,
+pub fn powerup_adder<T: Component + AddAssign + Copy>(
+    powerups: Query<(&Parent, &mut Powerup<T>)>,
+    mut guns: Query<&mut T>,
 ) {
     println!("powerup_adder");
     powerups.for_each(|(parent, powerup)| {
         if let Ok(mut gun) = guns.get_mut(parent.get()) {
-            gun.0 += powerup.value.0;
+            *gun += powerup.value;
         }
     });
 }
 
 #[test]
 fn did_add_rate_of_fire_to_gun_cooldown_rate() {
-
     let mut app = App::new();
 
-    let pu1 =  Powerup::<RateOfFire> {
+    let pu1 = Powerup::<RateOfFire> {
         value: RateOfFire(1),
     };
-    let pu2 =  Powerup::<RateOfFire> {
+    let pu2 = Powerup::<RateOfFire> {
         value: RateOfFire(2),
     };
-    let laser_gun = app.world.spawn(RateOfFire(0)).with_children(|parent| {
-        parent.spawn(pu1);
-        parent.spawn(pu2);
+    let laser_gun = app
+        .world
+        .spawn(RateOfFire(0))
+        .with_children(|parent| {
+            parent.spawn(pu1);
+            parent.spawn(pu2);
+        })
+        .id();
 
-    }).id();
-
-
-    app.add_system(powerup_adder);
+    app.add_system(powerup_adder::<RateOfFire>);
     app.update();
 
     let rate_of_fire = app.world.get::<RateOfFire>(laser_gun).unwrap();
