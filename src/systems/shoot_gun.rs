ee807227f98be2d793fd1d7f6dfdee6cd3e0ee3c
redaphid <iam@aaronherres.com>
2023-02-07 18:34:05 -0700

    don't underflow cooldown

--- src/systems/shoot_gun.rs
+++ src/systems/shoot_gun.rs
@@ -11,7 +11,7 @@ pub fn shoot_gun(
 ) {
     guns.for_each_mut(|(mut gun, transform)| {
         if gun.cooldown > 0 {
-            gun.cooldown -= gun.cooldown_rate;
+            gun.cooldown = gun.cooldown.saturating_sub(gun.cooldown_rate);
             return;
         }
 
