e2622964be48a0d001516cad8144793249cfb02e
redaphid <iam@aaronherres/com>
2023-02-13 00:22:40 -0700

    riddle me this, @royvandewater: Why does poison work like this?

--- src/systems/attach_poison.rs
+++ src/systems/attach_poison.rs
@@ -3,12 +3,12 @@ use crate::{components::{Poison, Math}};
 
 pub fn attach_poison(
     mut commands: Commands,
-    q_time_to_live: Query<Without<Poison>>,
+    q_no_poison: Query<Without<Poison>>,
     mut q_poison_powerup: Query<&Parent, With<Math<Poison>>>,
 ) {
 
     q_poison_powerup.for_each_mut(|parent| {
-        if let Ok(_) = q_time_to_live.get(parent.get()) {
+        if let Ok(_) = q_no_poison.get(parent.get()) {
             println!("attaching poison");
             commands.entity(parent.get()).insert(Poison::default());
         }
--- src/systems/poison.rs
+++ src/systems/poison.rs
@@ -4,14 +4,10 @@ use crate::components::{Health, Math, Poison, TimeToLive};
 
 pub fn poison(mut commands: Commands, q_poison: Query<(Entity, &Poison)>) {
     q_poison.for_each(|(entity, poison)| {
-        println!("poisoning for {}", poison.0);
         commands.entity(entity).with_children(|parent| {
-            parent
-                .spawn_empty()
-                .insert(Math::add(Health(-1)))
-                .with_children(|parent| {
-                    parent.spawn(Math::add(TimeToLive(10)));
-                });
+            parent.spawn(Math::add(Health(-1))).with_children(|p2| {
+                p2.spawn(Math::add(TimeToLive(10)));
+            });
         });
     });
 }
