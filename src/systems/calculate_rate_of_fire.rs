9f22766807dd4b6b5d38d3993a9c74a30f53bc6d
Roy van de Water <roy.v.water@gmail.com>
2023-03-02 17:38:12 -0700

    Simple refactor

--- src/systems/calculate_rate_of_fire.rs
+++ src/systems/calculate_rate_of_fire.rs
@@ -3,11 +3,10 @@ use bevy::prelude::*;
 
 pub fn calculate_rate_of_fire(
     mut rate_of_fires: Query<&RateOfFire, With<Children>>,
-    mut guns: Query<(&mut LaserGun,&Parent)>,
+    mut guns: Query<(&mut LaserGun, &Parent)>,
 ) {
     guns.for_each_mut(|(mut gun, parent)| {
-        if let Ok(rate_of_fire) = rate_of_fires.get_mut(parent.get()) {
-            gun.cooldown_rate = rate_of_fire.0 as usize;
-        }
+        let Ok(rate_of_fire) = rate_of_fires.get_mut(parent.get()) else { return; };
+        gun.cooldown_rate = rate_of_fire.0 as usize;
     });
 }
